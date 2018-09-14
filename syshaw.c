e64bd53f33ab5d9389adfae781d0b046c9fb3f94
yanminglai <yanminglai91@gwu.edu>
2018-09-13 21:47:00 -0400

    add syshaw as syscall

---
+++ syshaw.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[]){
+	haw();
+	exit();
+	return 0;
+}
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,9 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int 
+sys_haw(void){
+	 cprintf("Hello World\n");
+	 return 0;
+}
